c496f544c99892b7435a4f0dec95464b3e554bc5
Kyle Huang Junyuan <junyuan27@gmail.com>
2019-09-20 13:36:45 +0800

    Finalised P1 and P2 map descriptors.

--- algorithm/src/entities/Map.java
+++ algorithm/src/entities/Map.java
@@ -95,32 +95,33 @@ public class Map {
 	 */
 	public static String getP1Descriptors(Map map) {
 		String P1 = new String();
-		
+
 		P1 += "11";		// Padding sequence
-		
+
 		for (int y = 0; y < Map.maxY; y++) {
 			for (int x = 0; x < Map.maxX; x++) {
 				int cellType = map.getCell(new Coordinate(y, x)).getCellType();
-				
+
 				if (cellType == Cell.UNKNOWN)
 					P1 += "0";
 				else
 					P1 += "1";
 			}
 		}
-		
+
 		P1 += "11";		// Padding sequence
-		
+
 		// Convert to Hexadecimal
-//		for (int i = 0; i < 304; i+=4) {
-//			String stringOf4Bits = P1.charAt(i) + P1.charAt(i+1) + P1.charAt(i+2) + P1.charAt(i+3);
-//			int intOf4Bits = Integer.parseInt(, 2);	// Binary String to Decimal Number
-//			String hexString = Integer.toString(intOf4Bits, 16).toUpperCase();	// Decimal Number to Hex String
-//		}
-		
-		return P1;
+		String hexString = new String();
+		for (int i = 0; i < 304; i += 4) {
+			String testString = P1.substring(i, i + 4);
+			int intOf4Bits = Integer.parseInt(testString, 2);	// Binary String to Decimal Number
+			hexString += Integer.toString(intOf4Bits, 16).toUpperCase();	// Decimal Number to Hex String
+		}
+
+		return hexString;
 	}
-	
+
 	/**
 	 * Get P2 descriptors from a given <tt>Map</tt> object.
 	 * 
@@ -128,11 +129,11 @@ public class Map {
 	 */
 	public static String getP2Descriptors(Map map) {
 		String P2 = new String();
-		
+
 		for (int y = 0; y < Map.maxY; y++) {
 			for (int x = 0; x < Map.maxX; x++) {
 				int cellType = map.getCell(new Coordinate(y, x)).getCellType();
-				
+
 				if (cellType != Cell.UNKNOWN) {
 					if (cellType == Cell.WALL)
 						P2 += "1";
@@ -141,7 +142,39 @@ public class Map {
 				}
 			}
 		}
-		
-		return P2;
+
+		// Normalise P2 Binary
+		int remainder = P2.length() % 4;
+		String lastBit = new String();
+		String padding = new String();
+
+		switch (remainder) {
+		case 1:
+			lastBit = P2.substring(P2.length() - 1);
+			padding = "000";
+			P2 = P2.substring(0, P2.length() - 1).concat(padding).concat(lastBit);
+			break;
+		case 2:
+			lastBit = P2.substring(P2.length() - 2);
+			padding = "00";
+			P2 = P2.substring(0, P2.length() - 2).concat(padding).concat(lastBit);
+			break;
+		case 3:
+			lastBit = P2.substring(P2.length() - 3);
+			padding = "0";
+			P2 = P2.substring(0, P2.length() - 3).concat(padding).concat(lastBit);
+			break;
+		default: // Do nothing
+		}
+
+		// Convert to Hexadecimal
+		String hexString = new String();
+		for (int i = 0; i < P2.length(); i += 4) {
+			String testString = P2.substring(i, i + 4);
+			int intOf4Bits = Integer.parseInt(testString, 2);	// Binary String to Decimal Number
+			hexString += Integer.toString(intOf4Bits, 16).toUpperCase();	// Decimal Number to Hex String
+		}
+
+		return hexString;
 	}
 }
