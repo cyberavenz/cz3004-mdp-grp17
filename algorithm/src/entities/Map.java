de5925fb383069902c9fc9dc01853135b7fa66e9
Kyle Huang Junyuan <junyuan27@gmail.com>
2019-09-30 15:12:58 +0800

    First exploration test.

--- algorithm/src/entities/Map.java
+++ algorithm/src/entities/Map.java
@@ -6,7 +6,9 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
+import communications.TCPComm;
 import entities.Cell;
+import main.Main;
 
 public class Map {
 	public static final int maxY = 20;
@@ -89,18 +91,16 @@ public class Map {
 	}
 
 	/**
-	 * Get P1 descriptors from a given <tt>Map</tt> object.
-	 * 
-	 * @param map
+	 * Get P1 descriptors.
 	 */
-	public static String getP1Descriptors(Map map) {
+	public String getP1Descriptors() {
 		String P1 = new String();
 
 		P1 += "11";		// Padding sequence
 
 		for (int y = 0; y < Map.maxY; y++) {
 			for (int x = 0; x < Map.maxX; x++) {
-				int cellType = map.getCell(new Coordinate(y, x)).getCellType();
+				int cellType = this.getCell(new Coordinate(y, x)).getCellType();
 
 				if (cellType == Cell.UNKNOWN)
 					P1 += "0";
@@ -114,8 +114,8 @@ public class Map {
 		// Convert to Hexadecimal
 		String hexString = new String();
 		for (int i = 0; i < 304; i += 4) {
-			String testString = P1.substring(i, i + 4);
-			int intOf4Bits = Integer.parseInt(testString, 2);	// Binary String to Decimal Number
+			String binOf4Bits = P1.substring(i, i + 4);
+			int intOf4Bits = Integer.parseInt(binOf4Bits, 2);	// Binary String to Decimal Number
 			hexString += Integer.toString(intOf4Bits, 16).toUpperCase();	// Decimal Number to Hex String
 		}
 
@@ -123,16 +123,14 @@ public class Map {
 	}
 
 	/**
-	 * Get P2 descriptors from a given <tt>Map</tt> object.
-	 * 
-	 * @param map
+	 * Get P2 descriptors.
 	 */
-	public static String getP2Descriptors(Map map) {
+	public String getP2Descriptors() {
 		String P2 = new String();
 
 		for (int y = 0; y < Map.maxY; y++) {
 			for (int x = 0; x < Map.maxX; x++) {
-				int cellType = map.getCell(new Coordinate(y, x)).getCellType();
+				int cellType = this.getCell(new Coordinate(y, x)).getCellType();
 
 				if (cellType != Cell.UNKNOWN) {
 					if (cellType == Cell.WALL)
@@ -170,11 +168,92 @@ public class Map {
 		// Convert to Hexadecimal
 		String hexString = new String();
 		for (int i = 0; i < P2.length(); i += 4) {
-			String testString = P2.substring(i, i + 4);
-			int intOf4Bits = Integer.parseInt(testString, 2);	// Binary String to Decimal Number
+			String binOf4Bits = P2.substring(i, i + 4);
+			int intOf4Bits = Integer.parseInt(binOf4Bits, 2);	// Binary String to Decimal Number
 			hexString += Integer.toString(intOf4Bits, 16).toUpperCase();	// Decimal Number to Hex String
 		}
 
 		return hexString;
+	}
+
+	/**
+	 * Reveal cells around the <tt>Robot</tt> based on the current mode.<br>
+	 * <b>Simulation</b>: Must provide a <tt>testMap</tt>.<br>
+	 * <b>Real Run</b>: Read sensor values from Arduino.
+	 * 
+	 * @param robot
+	 * @param testMap Set to <tt>null</tt> for real run
+	 */
+	public void reveal(Robot robot, Map testMap) {
+		Sensor[] sensors = robot.getAllSensors();
+		Coordinate[] coordinates;
+
+		/* Real Run Mode */
+		if (Main.isRealRun) {
+			String message = Main.comms.readFrom(TCPComm.ARDUINO);
+			String[] arduinoSensors = message.split("[|]");	// Requires the use of [] character class
+
+			if (arduinoSensors.length != sensors.length) {
+				System.err.println("Incorrect sensor format received from Arduino!");
+				return;
+			}
+
+			System.out.print("Cleaned sensor values:");
+			// ORIGINAL: sensors.length
+			for (int i = 0; i < 5; i++) {
+				int arduinoSensorValue = Integer.parseInt(arduinoSensors[i]);
+
+				coordinates = sensors[i].getFacingCoordinates(robot);
+
+				if (coordinates != null) {
+					// If received value is above range, assume max depth
+					if (arduinoSensorValue > sensors[i].getDepth())
+						arduinoSensorValue = sensors[i].getDepth();
+
+					System.out.print(" " + arduinoSensorValue);
+
+					// Wall is detected
+					if (arduinoSensorValue < coordinates.length) {
+						for (int j = 0; j < coordinates.length; j++) {
+							if (j == arduinoSensorValue) {
+								this.getCell(coordinates[j]).setCellType(Cell.WALL);
+								break;	// Unable to see past any wall, break!
+							} else {
+								this.getCell(coordinates[j]).setCellType(Cell.PATH);
+							}
+						}
+					}
+
+					// No walls detected
+					else if (arduinoSensorValue == coordinates.length) {
+						for (int j = 0; j < coordinates.length; j++) {
+							this.getCell(coordinates[j]).setCellType(Cell.PATH);
+						}
+					}
+
+				}
+			}
+			System.out.println();
+		}
+
+		/* Simulation Mode */
+		else {
+			for (int i = 0; i < sensors.length; i++) {
+				coordinates = sensors[i].getFacingCoordinates(robot);
+
+				// Only when sensor sees some coordinates
+				if (coordinates != null) {
+					for (int j = 0; j < coordinates.length; j++) {
+						Cell unknownCell = this.getCell(coordinates[j]);
+						Cell simulatedCell = testMap.getCell(coordinates[j]);
+						unknownCell.setCellType(simulatedCell.getCellType());
+
+						// Sensor should not be able to see past walls
+						if (simulatedCell.getCellType() == Cell.WALL)
+							break;
+					}
+				}
+			}
+		}
 	}
 }
