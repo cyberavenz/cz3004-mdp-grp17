de10633b3b7aa4d5081c6d1c13607932e08d3214
Kyle Huang Junyuan <kyle@hjunyuan.com>
2019-10-08 11:52:30 +0800

    Added new cellType for waypoint.

--- algorithm/src/entities/Cell.java
+++ algorithm/src/entities/Cell.java
@@ -1,7 +1,8 @@
 package entities;
 
 public class Cell extends Coordinate {
-	public static final char UNKNOWN = 'A', WALL = 'B', START = 'C', GOAL = 'D', PATH = 'E', FINAL_PATH = 'F';
+	public static final char UNKNOWN = 'A', WALL = 'B', START = 'C', GOAL = 'D', PATH = 'E', FINAL_PATH = 'F',
+			WAYPOINT = 'G';
 //	public static final int UP_ARROW = 1, DOWN_ARROW = 2, RIGHT_ARROW = 3, LEFT_ARROW = 4, STOP = 5, ONE = 6, TWO = 7,
 //			THREE = 8, FOUR = 9, FIVE = 10, ALPHABET_A = 11, ALPHABET_B = 12, ALPHABET_C = 13, ALPHABET_D = 14,
 //			ALPHABET_E = 15;
